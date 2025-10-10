d3608f55543801e2a811c00b4f49aedb500b09c9
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-10 09:52:16 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+
+read -p "Sure? " c
+case $c in y|Y) ;; *) exit ;; esac
+
+#create env
+cd $path
+[ -f env ] || cp env.sample env
+nano env
+
+#install script
+cd /opt
+rm -r popmain
+mkdir popmain
+cd popmain
+curl -L https://pipe.network/p1-cdn/releases/latest/download/pop -o pop
+chmod +x pop
+
+cat <<EOF > dockerfile
+FROM ubuntu:24.04
+RUN apt-get update && apt-get install -y ca-certificates curl libssl-dev && rm -rf /var/lib/apt/lists/*
+WORKDIR /app
+COPY pop .
+RUN chmod 777 pop
+RUN chmod +x ./pop
+CMD ["./pop"]
+EOF
+
+docker build -t popnode .
