af69f06d6a24217168c99d22481c7ee5c31ce2f0
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-10 14:18:11 +0200

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,3 +1,3 @@
 docker exec popmain ./pop status
 docker exec popmain ./pop earnings
-#docker exec popmain curl -s http://localhost:8081/health/detailed | jq
+docker exec popmain curl -s http://localhost:8081/health/detailed | jq
